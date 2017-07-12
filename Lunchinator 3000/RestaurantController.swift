4f7793a11fed326dbc2a3e5d6b7a3f58d50d2933
pattyhackins <patrickridd@yahoo.com>
2017-07-11 22:47:00 -0600

    Created network call to  "getRestaurants()" in the RestaurantController.

--- Lunchinator 3000/RestaurantController.swift
+++ Lunchinator 3000/RestaurantController.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+
+class RestaurantController {
+    
+    static let sharedController = RestaurantController()
+    
+    let restaurantURLString = "https://interview-project-17987.herokuapp.com/api/restaurants"
+    
+    func getRestaurants(completion: @escaping ([Restaurant]?) -> Void) {
+        
+        guard let restaurantURL = URL(string: self.restaurantURLString) else {
+            completion(nil)
+            return
+        }
+        NetworkController.performRequestForURL(url: restaurantURL, httpMethod: .get) { (data, error) in
+            guard let data = data, let restaurantDictionaries = (try? JSONSerialization.jsonObject(with: data, options: .allowFragments)) as? [[String:AnyObject]] else {
+                print("Cant retrieve restaurants: \(String(describing: error?.localizedDescription))")
+                completion(nil)
+                return
+            }
+            
+            let restaurants = restaurantDictionaries.flatMap { Restaurant(restaurantDict: $0) }
+            completion(restaurants)
+        }
+    }
+    
+    
+    
+}
