41eda108c20b0b5d0d406291ff543b76afeeb298
pattyhackins <patrickridd@yahoo.com>
2017-07-11 21:37:50 -0600

    Setup basic MVC structure for Lunchinator and added some Models, Views, and Controller classes.

---
+++ Lunchinator 3000/NetworkController.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkController.swift
+//  Lunchinator 3000
+//
+//  Created by Patrick Ridd on 7/11/17.
+//  Copyright © 2017 PatrickRidd. All rights reserved.
+//
+
+import Foundation
---
+++ Lunchinator 3000/Review.swift
@@ -0,0 +1,9 @@
+//
+//  Review.swift
+//  Lunchinator 3000
+//
+//  Created by Patrick Ridd on 7/11/17.
+//  Copyright © 2017 PatrickRidd. All rights reserved.
+//
+
+import Foundation
